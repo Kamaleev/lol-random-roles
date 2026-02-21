bba75977861a50615cf1ed5e8cb91572950b5805
Камалеев Руслан <r.kamaleev@vk.team>
2026-02-22 01:12:22 +0300

    view build

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('root');
+  const root = createRoot(container as HTMLElement);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
